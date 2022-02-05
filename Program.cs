71223b46273d49f65cd6f6b323efd47f4df03784
Davi Borges <davifelipe_300@hotmail.com>
2022-02-05 10:52:29 -0300

    Contador numerico com instrução break

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExercicioContadorInstrucaoBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int numeroInicial;
+            int numeroFinal;
+
+            Console.WriteLine("Digite o numero inicial da contagem: ");
+            numeroInicial = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Digite o numero final da contagem: ");
+            numeroFinal = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {   
+                if (numeroInicial > numeroFinal)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{numeroInicial}");
+
+                numeroInicial = numeroInicial + 1;
+                                
+            }
+
+        }
+    }
+}
